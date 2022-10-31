2b2f9ea986085cdd2482636918ac2fe19f50d0eb
surinoel <taolaminh9@gmail.com>
2022-11-01 02:23:08 +0700

    Rework

--- resources/views/Backend/layouts/index.blade.php
+++ resources/views/Backend/layouts/index.blade.php
@@ -72,10 +72,10 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy;<a href="http://adminlte.io"></a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.4
+      <b></b>
     </div>
   </footer>
 
